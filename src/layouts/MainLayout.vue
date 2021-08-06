a6c52af5185f723b7b4a35a20e7a9702761a3666
Josh.5 <jsunnex@gmail.com>
2021-08-06 17:00:31 +1200

    Move the main menu drawer to the left hand side. It feels better here and will allow the right hand side to be used by other components later on

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,46 +3,37 @@
 
     <q-header reveal elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar rounded size="2rem" font-size="82px">
-
-            <img src="~assets/unmanic-logo-white.png">
-          </q-avatar>
-        </q-toolbar-title>
-
         <q-btn
           v-if="$route.meta.showDrawer"
           dense
           flat
           round
           icon="menu"
-          @click="toggleRightDrawer"/>
+          @click="toggleLeftDrawer"/>
         <q-btn
           v-if="!$route.meta.showDrawer"
           dense
           flat
           round
           @click="$router.push('/unmanic-dashboard')"
-          icon="arrow_forward">
+          icon="arrow_back">
         </q-btn>
-      </q-toolbar>
 
-      <!--
-            &lt;!&ndash;TODO: Remove styling once new frontend is 100% in use&ndash;&gt;
-            <q-tabs inline-label align="left" class="fit" style="min-height:64px">
-              <q-route-tab to="/unmanic-dashboard" :label="$t('navigation.dashboard')" icon="home" style="min-width:190px"/>
-              <q-route-tab to="/unmanic-settings" :label="$t('navigation.settings')" icon="settings"/>
-              <q-route-tab to="/unmanic-plugins" :label="$t('navigation.plugins')" icon="extension"/>&ndash;&gt;
-              <q-route-tab to="/settings/" exact :label="$t('navigation.settings')" icon="settings" style="min-width:190px"/>
-              <q-route-tab to="/plugins/" exact :label="$t('navigation.plugins')" icon="extension" style="min-width:190px"/>
-            </q-tabs>
-      -->
+        <q-toolbar-title>
+          <q-avatar rounded size="2rem" font-size="82px">
+            <img src="~assets/unmanic-logo-white.png">
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
 
     </q-header>
 
     <q-drawer
       v-if="$route.meta.showDrawer"
-      v-model="rightDrawerOpen" side="right" overlay elevated>
+      v-model="leftDrawerOpen"
+      side="left"
+      behavior="mobile"
+      overlay elevated>
       <Drawer/>
     </q-drawer>
 
@@ -71,12 +62,12 @@ import FooterData from "components/FooterData";
 export default {
   components: { Drawer, FooterData },
   setup() {
-    const rightDrawerOpen = ref(false)
+    const leftDrawerOpen = ref(false)
 
     return {
-      rightDrawerOpen,
-      toggleRightDrawer() {
-        rightDrawerOpen.value = !rightDrawerOpen.value
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
   }
