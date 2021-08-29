ad52e721d4989a04f3a8524130f7ef6276e480b7
Josh.5 <jsunnex@gmail.com>
2021-08-29 14:10:09 +1200

    Add script for running translations from google translate (requires API key in bash env)

---
+++ scripts/translate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# -
+# File: translate.sh
+# Project: unmanic-frontend
+# File Created: Saturday, 28th August 2021 2:04:46 pm
+# Author: Josh.5 (jsunnex@gmail.com)
+# -----
+# Last Modified: Sunday, 29th August 2021 2:09:04 pm
+# Modified By: Josh.5 (jsunnex@gmail.com)
+# -
+
+
+project_root="$(readlink -e $(dirname $(readlink -e ${BASH_SOURCE[0]}))/../)"
+
+languages_to_support="mi,zh,nl,fr,de,it,ja,ru,es,sv"
+
+if [[ -z ${GOOGLE_TRANSLATE_API_KEY} ]]; then
+    echo " Set the Google Translate API key in your bash variables before running. Exit!"
+    exit 1
+fi
+
+
+pushd "${project_root}" &> /dev/null
+
+i18n-translate-json ${GOOGLE_TRANSLATE_API_KEY} ./src/language/ en "${languages_to_support}"
+
+popd &. /dev/null
