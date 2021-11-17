cf4a4e6005658898e47b994fc3f21d3dff12446e
Adam Brown <adam@deftnerd.com>
2021-11-16 19:50:20 -0500

    Update FooterData.vue
    * Reorganized footer so it uses horizontal space better and is more compact. 
    * Updated copyright date(s)
    * Added link from version string to Github release page
    * Added link from authors name to their Github user profile
    * Added a link after the authors name to their "Buy me a coffee" page

--- src/components/FooterData.vue
+++ src/components/FooterData.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="row">
-    <div class="col-12 col-md-6">
+    <div class="col">
       <p class="text-subtitle1">
-        <br>
-        Version - {{ unmanicVersion }}
-        <br>
-        2018 © Unmanic by Josh Sunnex.
+        &copy; 2018-2021 by <a href="https://github.com/Josh5">Josh Sunnex</a> (<a href="https://ko-fi.com/josh5coffee">Buy Me a Coffee!</a>)
+      </p>
+    </div>
+    <div class="col-auto">
+      <p class="text-subtitle1">
+        Unmanic Version <a href="https://github.com/unmanic/unmanic/releases">{{ unmanicVersion }}</a>
       </p>
     </div>
   </div>
