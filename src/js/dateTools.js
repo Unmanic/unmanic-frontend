e3297783dcefe1bf6006c5d016da523593d16786
Andrew Smith <espadav8@gmail.com>
2024-01-11 16:04:09 +1100

    Reduce code duplication
    Signed-off-by: Andrew Smith <espadav8@gmail.com>

--- src/js/dateTools.js
+++ src/js/dateTools.js
@@ -1,17 +1,13 @@
 import { date } from 'quasar';
 
 export default {
-  printTimeSinceDate(startDate) {
-    let date_now = new Date();
+  printSecondsAsDuration(timestamp) {
+    timestamp = Number(timestamp);
 
-    let seconds = Math.floor((date_now - (startDate)) / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
-
-    hours = hours - (days * 24);
-    minutes = minutes - (days * 24 * 60) - (hours * 60);
-    seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+    const days = Math.floor(timestamp / 3600 / 24);
+    const hours = Math.floor((timestamp / 3600) % 24);
+    const minutes = Math.floor((timestamp % 3600) / 60);
+    const seconds = Math.floor((timestamp % 3600) % 60);
 
     const dDisplay = days > 0 ? days + (days == 1 ? " day, " : " days, ") : "";
     const hDisplay = hours > 0 ? hours + (hours == 1 ? " hour, " : " hours, ") : "";
@@ -19,17 +15,6 @@ export default {
     const sDisplay = seconds > 0 ? seconds + (seconds == 1 ? " second" : " seconds") : "";
 
     return dDisplay + hDisplay + mDisplay + sDisplay;
-  },
-  printTimeAsHoursMinsSeconds(timestamp) {
-    timestamp = Number(timestamp);
-    let hours = Math.floor(timestamp / 3600);
-    let minutes = Math.floor(timestamp % 3600 / 60);
-    let seconds = Math.floor(timestamp % 3600 % 60);
-
-    let hDisplay = hours > 0 ? hours + (hours == 1 ? " hour, " : " hours, ") : "";
-    let mDisplay = minutes > 0 ? minutes + (minutes == 1 ? " minute, " : " minutes, ") : "";
-    let sDisplay = seconds > 0 ? seconds + (seconds == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay;
   },
   printDateTimeString(dateTime) {
     let d = new Date(0);
