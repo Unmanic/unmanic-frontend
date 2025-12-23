274e57a098d3b1cb52b06411ab36c0c77b3b274b
Josh.5 <jsunnex@gmail.com>
2025-12-24 10:00:44 +1300

    Move the PluginsInstalledTable component

--- src/pages/SettingsPlugins.vue
+++ src/pages/SettingsPlugins.vue
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-import PluginsInstalledTable from "components/PluginsInstalledTable";
+import PluginsInstalledTable from "components/settings/plugins/partials/PluginsInstalledTable";
 import { UnmanicWebsocketHandler } from "src/js/unmanicWebsocket";
 import { onMounted, onUnmounted } from "vue";
 import { useI18n } from "vue-i18n";
